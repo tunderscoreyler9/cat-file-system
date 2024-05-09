fde03e0c97f19aac4eea7d80ac90e1d613747d0c
Tyler Skidmore <tyler.s9@proton.me>
2024-05-09 11:47:45 -0400

    Test commit

---
+++ step1.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err, data) {
+        if(err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.log(`File contents: ${data}`);
+    });
+};
+cat('one.txt');
+cat('one.txt');
